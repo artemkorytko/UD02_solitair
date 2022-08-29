e328853cfca872c64b0472b1d4b4565c3e845b59
Zakhar <z.zahurski@gmail.com>
2022-08-29 22:10:45 +0300

    add start base logic

--- Assets/Scripts/CardPlace.cs
+++ Assets/Scripts/CardPlace.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 
 public class CardPlace : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] protected int nextCardValue = -1;
+    [SerializeField] protected float onGameZOffset = -2f;
+    [SerializeField] protected CardColor nextCardColor = CardColor.Any;
+    [SerializeField] protected CardType nextCardType = CardType.Any;
+    [SerializeField] protected Transform cardContainer;
+    [SerializeField] protected bool isMain;
 
-    // Update is called once per frame
-    void Update()
+    protected bool isOpen = true;
+
+    public int NextCardValue => nextCardValue;
+
+    public CardColor NextCardColor => nextCardColor;
+
+    public CardType NextCardType => nextCardType;
+
+    public Transform CardContainer => cardContainer;
+
+    public bool IsMain => isMain;
+
+    public bool IsOpen => isOpen;
+
+    public bool IsCanConnect(PlayingCard playingCard)
     {
-        
+        if (!isOpen) 
+            return false;
+        if (playingCard.Value != nextCardValue && nextCardValue != -1) 
+            return false;
+        if (nextCardColor != CardColor.Any && playingCard.Color != nextCardColor) 
+            return false;
+        if (nextCardType != CardType.Any && playingCard.Type != nextCardType) 
+            return false;
+
+        Vector3 position = playingCard.CardContainer.transform.localPosition;
+        position.z = isMain ? 0f : onGameZOffset;
+        playingCard.CardContainer.localPosition = position;
+        return true;
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,74 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+namespace DefaultNamespace
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private int mainPlaceCompleteValue = 52;
+        [SerializeField] private CardPlace[] gameCardPlaces;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private CardDeck _cardDeck;
+        private PlayerController _playerController;
+        private readonly Dictionary<CardType, int> _mainPlaceInfo = new Dictionary<CardType, int>();
+
+        public event Action OnWin;
+
+        private void Awake()
+        {
+            _playerController = FindObjectOfType<PlayerController>();
+            _cardDeck = FindObjectOfType<CardDeck>();
+        }
+
+        private void Start()
+        {
+            GenerateField();
+            _playerController.OnAddToMain += OnAddToMain;
+            _playerController.OnRemoveFromMain += OnRemoveFromMain;
+        }
+
+        private void OnDestroy()
+        {
+            _playerController.OnAddToMain -= OnAddToMain;
+            _playerController.OnRemoveFromMain -= OnRemoveFromMain;
+        }
+
+        private void GenerateField()
+        {
+            _cardDeck.Initialize();
+            FillGamePlaces();
+        }
+
+        private void FillGamePlaces()
+        {
+            for (int i = 0; i < gameCardPlaces.Length; i++)
+            {
+                int counter = i;
+                CardPlace cardPlace = gameCardPlaces[i];
+                PlayingCard card = null;
+
+                while (counter > 0)
+                {
+                    card = _cardDeck.GetCard();
+                    card.SetParent(cardPlace);
+                    card.Close();
+                    cardPlace = card;
+                    counter--;
+                }
+
+                card = _cardDeck.GetCard();
+                card.SetParent(cardPlace);
+                card.Open();
+            }
+        }
+
+        private void OnRemoveFromMain(CardType type, int value)
+        {
+        }
+
+        private void OnAddToMain(CardType type, int value)
+        {
+        }
     }
 }
